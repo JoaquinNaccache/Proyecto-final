b2903c6cba5de8405bb32910a7a4a9cb98932ff7
JOAQUIN NACCACHE <47807062@ort>
2024-05-31 10:25:06 -0300

    primer creacion

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoFinal1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
