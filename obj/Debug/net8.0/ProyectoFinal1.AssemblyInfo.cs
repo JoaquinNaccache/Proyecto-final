c7b082ef5ecb1232f9664de340c4e7659c101d42
roni <47867214@ort>
2024-06-28 08:37:12 -0300

    asd

--- obj/Debug/net8.0/ProyectoFinal1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoFinal1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ed9f3fed264593922324e9b5e4a40e417fb0ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08047c38d72b34ddd2b711e11305fe2b929c41f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
