88683b983e4d045fddc4c6c3157408038ef89acf
JOAQUIN NACCACHE <47807062@ort>
2024-07-12 09:33:02 -0300

    demo 1 aprobada

--- obj/Debug/net8.0/ProyectoFinal1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoFinal1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c687d84ee75ade1316fe179175f018d4ebd347c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd3e4e813bc4679d26aea7f7b69fb33dfaf27f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
