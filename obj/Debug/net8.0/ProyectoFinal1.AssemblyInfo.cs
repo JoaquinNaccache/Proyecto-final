d9469626d379a2e10f2badc782931f7a66b9bda6
Android Studio <devandroid@ort>
2024-11-22 10:40:07 -0300

    funciona login y crear curso

--- obj/Debug/net8.0/ProyectoFinal1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoFinal1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b94ae26914a604e84f5158d75fbfd0183a590b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c306bc5280ad7dbd1d8d81a0d0df6f060cc875c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
